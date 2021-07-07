8e1d1c89783558794cc5eeb1baa73010e2024505
pawel_admin <ptcebula@gmail.com>
2021-07-07 20:09:39 +0200

    Completed 8.8-8.9

---
+++ part8/library-frontend/src/App.js
@@ -0,0 +1,39 @@
+import React, { useState } from 'react'
+import { useQuery } from '@apollo/client'
+import Authors from './components/Authors'
+import Books from './components/Books'
+import NewBook from './components/NewBook'
+import { ALL_AUTHORS, ALL_BOOKS } from './queries'
+
+const App = () => {
+  const [page, setPage] = useState('')
+  const booksResult = useQuery(ALL_BOOKS)
+  const authorsResult = useQuery(ALL_AUTHORS)
+
+  return (
+    <div>
+      <div>
+        <button onClick={() => setPage('authors')}>authors</button>
+        <button onClick={() => setPage('books')}>books</button>
+        <button onClick={() => setPage('add')}>add book</button>
+      </div>
+
+      <Authors
+        show={page === 'authors'}
+        authors={authorsResult.data ? authorsResult.data.allAuthors : []}
+      />
+
+      <Books
+        show={page === 'books'}
+        books={booksResult.data ? booksResult.data.allBooks : []}
+      />
+
+      <NewBook
+        show={page === 'add'}
+      />
+
+    </div>
+  )
+}
+
+export default App
---
+++ part8/library-frontend/src/queries.js
@@ -0,0 +1,23 @@
+import { gql } from '@apollo/client'
+
+export const ALL_AUTHORS = gql`
+  query {
+    allAuthors {
+      name
+      id
+      born
+      bookCount
+    }
+  }
+`
+
+export const ALL_BOOKS = gql`
+  query {
+    allBooks {
+      title
+      id
+      published
+      author
+    }
+  }
+`
